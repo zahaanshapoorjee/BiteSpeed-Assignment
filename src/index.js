24412cac2c585a5a5255b1ea5fd0bbbc8faaf616
zahaanshapoorjee <zahaan@shapoorjee.com>
2025-06-29 23:45:05 +0530

    receiving and logging email phone number in identify post endpoint

--- src/index.js
+++ src/index.js
@@ -2,12 +2,21 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json({ limit: '10mb' }));
+
 app.get("/", (req, res) =>{
     res.send("Assignment Setup Complete!");
 })
 
 app.post("/identify", (req, res) =>{
-    res.send("Identify Endpoint");
+    const { email, phoneNumber} = req.body;
+    res.status(200).json({
+        message: "Data received by identify endpoint",
+        data: {
+            email: email? email: "No email provided",
+            phoneNumber: phoneNumber? phoneNumber: "No phone number provided"
+        }
+    })
 })
 
 app.listen(port, ()=>{
