badfeb70bf07b89c00303f0bfa6a5ce0a1487797
zahaanshapoorjee <zahaan@shapoorjee.com>
2025-06-30 03:59:51 +0530

    Split up into 2 routes, for ease of reviewer, one is the main assignment code, the other is get /reset that will help reset database back to single row state as shown in example of assignment instructions

--- src/index.js
+++ src/index.js
@@ -1,29 +1,33 @@
 const express = require('express');
+require('dotenv').config();
+
+// Initializing Express 
 const app = express();
 const port = process.env.PORT || 3000;
 
+//as per the instructions, I am parsing jsons, limiting 10mb for safety
 app.use(express.json({ limit: '10mb' }));
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.send("Assignment Setup Complete!");
-})
+});
 
-app.post("/identify", (req, res) =>{
-    const { email, phoneNumber} = req.body;
-    var primaryContactIds = []; // this is what we are going to fetch from mysql db
-    var secondaryContactIds = []; // this as well
-    var emails = []; // same as above
-    var phoneNumbers = []; // same as above
-    res.status(200).json({
-        contact: {
-            "primaryContatctId": primaryContactIds,
-			"emails": emails, // first element being email of primary contact 
-			"phoneNumbers": phoneNumbers, // first element being phoneNumber of primary contact
-			"secondaryContactIds": secondaryContactIds // Array of all Contact IDs that are "secondary" to the primary contact
-        }
-    })
-})
+// Ive made 2 routes, one as per the instructions, the identify route, you have given (I am speaking to whoever at BiteSpeed is reviewing the code), and another to reset the MySQL database
+// back to its original form with only one entry, just like in the instructions:
+//{
+// 	id                   1                   
+//   phoneNumber          "123456"
+//   email                "lorraine@hillvalley.edu"
+//   linkedId             null
+//   linkPrecedence       "primary"
+//   createdAt            2023-04-01 00:00:00.374+00              
+//   updatedAt            2023-04-01 00:00:00.374+00              
+//   deletedAt            null
+// }
 
-app.listen(port, ()=>{
+app.use(require('./routes/identify'));
+app.use(require('./routes/reset'));
+
+app.listen(port, () => {
     console.log(`Server setup and ready on port ${port}`);
-})
+});
